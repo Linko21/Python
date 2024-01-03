91aa4b7fb535e66d492164dcd5ae827d3878c892
Linko <Linko@DESKTOP-44B0AK2.fritz.box>
2024-01-03 03:40:36 +0100

    Version 2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,23 @@
 from openpyxl import Workbook
-wb= Workbook()
-#ws=wb.active
+#from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.reader.excel import load_workbook
 
+wb= Workbook()
 def createFile():
     a=input('Bitte geben Sie einen Dateinamen ein', )
-    b= a+'.xlsx'
-
+    b= a+'.xlsx' 
     wb.save(b)
+    
+def Spaltennamen():
+    c=input('Bitte geben den Dateinamen an', )
+    d=c+'.xlsx'
+    wb=load_workbook(d)
+    ws=wb.active
+    ws.title='Geburtsdatum'
+    ws['A1'].value = 'Name'
+    ws['B1'].value = 'Geburtsdatum'  
+    wb.save(d)
 
-#ws.title='Geburtsdatum'
-#ws['A1'].value = 'Name'
-#ws['B1'].value = 'Geburtsdatum'  
+
 createFile()
+Spaltennamen()
