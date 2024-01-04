c2706ac1c16a287dbf129e265f03646df41c7a06
Stephan.Praetsch <stephan.praetsch@unite.eu>
2024-01-04 19:07:59 +0100

    iterate over rows and cols

--- HelloWorld.py
+++ HelloWorld.py
@@ -26,6 +26,10 @@ def row_and_col_writer():
     ws.title ='Geburtsdatum'
     ws['A1'].value = 'Name'
     ws['B1'].value = 'Geburtsdatum'  
+    ws['A2'].value = 'Iche'
+    ws['B2'].value = 'heute'
+    ws['C1'].value = 'Irgendwas'
+    ws['C2'].value = 'Toll'
     wb.save('Test.xlsx')
 
 def row_and_col_reader():
@@ -35,8 +39,26 @@ def row_and_col_reader():
         for col in range(1,3):
             z = ws['A' + str(col)].value
             print(r, z)
-   
+
+def row_and_col_reader_2():
+    print("with array")
+    ws = wb.active
+    for row in range(1,3):
+        row_index = str(row)
+        for col_index in ['A', 'B', 'C']:
+            v = ws[col_index+row_index].value
+            print("["+col_index+","+row_index+"] = "+v)
+
+def internet_solution():
+    print("internet_solution")
+    sheet = load_workbook('Test.xlsx').active
+    for i in range(1, sheet.max_row+1):
+        row = [cell.value for cell in sheet[i]]
+        print("row "+str(i), row)
+
 create_empty_excel_file()
 load_excel_file()
 row_and_col_writer()
 row_and_col_reader()
+row_and_col_reader_2()
+internet_solution()
