e21120397e85e4d03d7f9053b9a1f700b45f60bf
Linko <Linko@DESKTOP-44B0AK2.fritz.box>
2024-01-08 19:27:07 +0100

    änderung row and col reader

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,15 @@
 from openpyxl import Workbook
 from openpyxl.reader.excel import load_workbook
-#from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.xml.constants import MAX_ROW, MAX_COLUMN, MIN_COLUMN, MIN_ROW
 #import os
 
 wb = Workbook()
 ws = wb
+
 def create_empty_excel_file():  
-    a = input('Bitte geben Sie einen Dateinamen ein\n')
-    b = a+'.xlsx'
+    #a = input('Bitte geben Sie einen Dateinamen ein\n')
+    b = 'Test.xlsx' #a+'.xlsx'
     wb.save(b)
     return(b)
 #    print('path: '+os.path.abspath(b))
@@ -25,17 +27,28 @@ def row_and_col_writer():
     ws = wb.active
     ws.title ='Geburtsdatum'
     ws['A1'].value = 'Name'
-    ws['B1'].value = 'Geburtsdatum'  
+    ws['B1'].value = 'Geburtsdatum'
+    ws['A2'].value = 'Name'
+    ws['B2'].value = 'Geburtsdatum'
+    #ws['A50'].value = 120
+    #ws['B100'].value = 900  
     wb.save('Test.xlsx')
 
 def row_and_col_reader():
     ws = wb.active
-    for row in range(1,3):
-        r = ws['A' + str(row)].value
-        for col in range(1,3):
-            z = ws['A' + str(col)].value
-            print(r, z)
-   
+#    for row in range(MIN_ROW+1,5):
+#        r = ws ['A'+str(row)].value    
+#        for col in range(MIN_COLUMN+1,5):
+#            z = ws['A'+str(col)].value
+#            print(r, z)
+    for i in range(1, wb.active.max_row+1):
+        row = [cell.value for cell in wb.active[i]]
+        print("In Zeile "+str(i), row)
+        cell = row
+        if cell.value is None:
+            cell.value = 'Eintrag'
+            break        
+        
 create_empty_excel_file()
 load_excel_file()
 row_and_col_writer()
