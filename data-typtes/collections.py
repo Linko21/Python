0a65475986821b1289a07627a54ba7b2724e7b2e
Stephan.Praetsch <stephan.praetsch@unite.eu>
2024-02-23 21:22:20 +0100

    add examples for list, set and dictionary

---
+++ data-typtes/collections.py
@@ -0,0 +1,18 @@
+numbers = [19, 19, 19, 26, 29]
+print("list numbers", numbers)
+print("set numbers", set(numbers))
+
+languages = ["german", "german", "french", "french", "chinese", "portuguese", "portuguese"]
+print("list languages", list(languages))
+print("set languages", set(languages))
+
+shenanigans = list("shenanigans")
+print("list shenanigans", shenanigans)
+print("set shenanigans", set(shenanigans))
+
+dictionary = {
+    "numbers": numbers,
+    "languages": languages,
+    "shenanigans": shenanigans
+}
+print("dictionary", dictionary)
