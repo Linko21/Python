69961fda53b3d69345370a537a3cda223093076c
Linko <Linko@DESKTOP-44B0AK2.fritz.box>
2024-03-22 15:04:42 +0100

    hier hab ich nur was getestet

--- data-typtes/collections.py
+++ data-typtes/collections.py
@@ -1,18 +1,28 @@
-numbers = [19, 19, 19, 26, 29]
-print("list numbers", numbers)
-print("set numbers", set(numbers))
+#numbers = [19, 19, 19, 26, 29]
+#print("list numbers", numbers)
+#print("set numbers", set(numbers))
 
-languages = ["german", "german", "french", "french", "chinese", "portuguese", "portuguese"]
-print("list languages", list(languages))
-print("set languages", set(languages))
+#keys = set()
+#print('keys1', keys)
+#keys.add('Hallo')
+#print('keys2', keys)
+#languages = ["german", "german", "french", "french", "chinese", "portuguese", "portuguese"]
+#keys.update(languages)
+#print('keys3', keys)
+#keys = sorted(keys) 
+#print('keys4', keys)
 
-shenanigans = list("shenanigans")
-print("list shenanigans", shenanigans)
-print("set shenanigans", set(shenanigans))
+
+#print("list languages", list(languages))
+#print("set languages", set(languages))
+
+#shenanigans = list("shenanigans")
+#print("list shenanigans", shenanigans)
+#print("set shenanigans", set(shenanigans))
 
 dictionary = {
-    "numbers": numbers,
-    "languages": languages,
-    "shenanigans": shenanigans
+    "numbers": "numbers",
+    "languages": "languages",
+    "shenanigans": "shenanigans"
 }
 print("dictionary", dictionary)
