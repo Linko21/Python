ada9481ebd349ca6d29c39a4aa9c1a33fd42ba61
Stephan.Praetsch <stephan.praetsch@unite.eu>
2024-05-04 18:20:04 +0200

    clean up

--- cards/cards_handler.py
+++ cards/cards_handler.py
@@ -105,6 +105,6 @@ def add_values_to_cell(card, row, ws):
 def handle():
     cards_in_local_file_as_json = download_data_from_url_to_file("https://api.lorcana-api.com/cards/all", "cards/cards.json")
     cards = read_cards_file_content(cards_in_local_file_as_json)
-    sets_in_local_file_as_json = download_data_from_url_to_file("https://api.lorcana-api.com/sets/all", "cards/sets.json")
-    card_sets = read_sets_file_content(sets_in_local_file_as_json)
+    card_sets_in_local_file_as_json = download_data_from_url_to_file("https://api.lorcana-api.com/sets/all", "cards/card_sets.json")
+    card_sets = read_sets_file_content(card_sets_in_local_file_as_json)
     write_cards_to_excel(cards, card_sets, "cards/cards.xlsx")
--- cards/downloader.py
+++ cards/downloader.py
@@ -10,10 +10,10 @@ log = logging.getLogger(__name__)
 
 def download_data_from_url_to_file(url, target):
     if os.path.isfile(target):
-        os.stat(target)
         log.info("file {} already exists, downloaded at {}, reusing this file".format(os.path.abspath(target), time.ctime(os.path.getmtime(target))))
         return target
-    plain_data = download(url)
+    log.info("downloading " + url)
+    plain_data = requests.get(url)
     json_object = json.loads(plain_data.text)
     json_formatted_str = json.dumps(json_object, indent=2)
     f = open(target, "w")
@@ -21,10 +21,3 @@ def download_data_from_url_to_file(url, target):
     f.close()
     log.info("downloaded {} to {}".format(url, os.path.abspath(target)))
     return target
-
-
-def download(url: str):
-    log.info("downloading " + url)
-    resp = requests.get(url)
-    log.info("downloaded " + url)
-    return resp
